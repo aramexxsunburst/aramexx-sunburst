808bb420e7bc07faa16d5de8bc950a93f6af668a
Xander Harris <cfo@gahan-corporation.com>
2018-03-24 16:04:21 -0700

    Update heroku apps. See #5.

--- terraform/heroku.tf
+++ terraform/heroku.tf
@@ -1,32 +1,32 @@
 variable "heroku_api_key" {}
 
 provider "heroku" {
-  email = "xander@gahancorporation.com"
+  email = "aramexx.sunburst@gmail.com"
   api_key = "${var.heroku_api_key}"
 }
 
 resource "heroku_app" "staging" {
-  name = "aramexx-sunburst-staging"
+  name = "aramexx-sunburst-stage"
   region = "us"
 }
 
 resource "heroku_app" "production" {
-  name = "aramexx-sunburst-production"
+  name = "aramexx-sunburst-prod"
   region = "us"
 }
 
-resource "heroku_pipeline" "aramexx-sunburst-pipeline" {
-  name = "aramexx-sunburst-pipeline"
+resource "heroku_pipeline" "aramexx-sunburst-pipe" {
+  name = "aramexx-sunburst-pipe"
 }
 
 resource "heroku_pipeline_coupling" "staging" {
   app      = "${heroku_app.staging.name}"
-  pipeline = "${heroku_pipeline.aramexx-sunburst-pipeline.id}"
+  pipeline = "${heroku_pipeline.aramexx-sunburst-pipe.id}"
   stage    = "staging"
 }
 
 resource "heroku_pipeline_coupling" "production" {
   app      = "${heroku_app.production.name}"
-  pipeline = "${heroku_pipeline.aramexx-sunburst-pipeline.id}"
+  pipeline = "${heroku_pipeline.aramexx-sunburst-pipe.id}"
   stage    = "production"
 }
