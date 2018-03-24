d0e6d525c6e1e4612719957af95dbfed0839e8f3
Xander Harris <cfo@gahan-corporation.com>
2018-03-24 15:33:58 -0700

    Update heroku. See #5.

--- terraform/heroku.tf
+++ terraform/heroku.tf
@@ -5,6 +5,14 @@ provider "heroku" {
   api_key = "${var.heroku_api_key}"
 }
 
+resource "heroku_app" "staging" {
+  name = "aramexx-sunburst-staging"
+}
+
+resource "heroku_app" "production" {
+  name = "aramexx-sunburst-production"
+}
+
 resource "heroku_pipeline" "aramexx-sunburst-pipeline" {
   name = "aramexx-sunburst-pipeline"
 }
