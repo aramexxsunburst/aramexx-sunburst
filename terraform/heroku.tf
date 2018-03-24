639277508ad5b92c5d37e9cbffc3fb790e8d223b
Xander Harris <cfo@gahan-corporation.com>
2018-03-24 15:35:33 -0700

    Add region and coupling. See #5.

--- terraform/heroku.tf
+++ terraform/heroku.tf
@@ -7,12 +7,26 @@ provider "heroku" {
 
 resource "heroku_app" "staging" {
   name = "aramexx-sunburst-staging"
+  region = "us"
 }
 
 resource "heroku_app" "production" {
   name = "aramexx-sunburst-production"
+  region = "us"
 }
 
 resource "heroku_pipeline" "aramexx-sunburst-pipeline" {
   name = "aramexx-sunburst-pipeline"
+}
+
+resource "heroku_pipeline_coupling" "staging" {
+  app      = "${heroku_app.staging.name}"
+  pipeline = "${heroku_pipeline.aramexx-sunburst-staging-app.id}"
+  stage    = "staging"
+}
+
+resource "heroku_pipeline_coupling" "production" {
+  app      = "${heroku_app.production.name}"
+  pipeline = "${heroku_pipeline.aramexx-sunburst-production-app.id}"
+  stage    = "production"
 }
