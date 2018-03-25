1ded0bbba33d120c0d098354c6ba2fc42728918f
Xander Harris <cfo@gahan-corporation.com>
2018-03-24 17:25:03 -0700

    Enable postgres. See #11.

--- terraform/docker.tf
+++ terraform/docker.tf
@@ -1,3 +1,12 @@
 provider "docker" {
-  host = "tcp://127.0.0.1:2376/"
+  host = "tcp://127.0.0.1:2375/"
+}
+
+resource "docker_container" "postgres" {
+  image = "postgres:alpine"
+  name  = "postgres"
+  ports {
+    internal = 5432
+    external = 5432
+  }
 }
