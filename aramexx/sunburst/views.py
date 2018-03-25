25ab4191f317085e5d0a781be692c1efeafb5baa
Xander Harris <cfo@gahan-corporation.com>
2018-03-25 14:07:07 -0700

    Start upload form. See #17.

--- aramexx/sunburst/views.py
+++ aramexx/sunburst/views.py
@@ -1,7 +1,21 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .forms import UploadForm
 
 # Create your views here.
 
 def index(request):
     context = {}
     return render(request, 'sunburst/index.html', context)
+
+
+def upload(request):
+    if request.method == 'POST':
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            print(request.FILES['file'])
+            # import_xml_file(request.FILES['file'])
+            return HttpResponseRedirect('/')
+    else:
+        form = UploadForm()
+    return render(request, 'sunburst/upload.html', {'form': form})
