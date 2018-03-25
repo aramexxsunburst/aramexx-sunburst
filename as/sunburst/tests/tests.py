98b1bb5793c499e29a96962a4b9b282c3ec84c0a
Xander Harris <cfo@gahan-corporation.com>
2018-03-24 17:10:51 -0700

    Add tests. See #10.

---
+++ as/sunburst/tests/tests.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Test case for home.views."""
+from django.test import Client
+from django.urls import reverse
+
+from home.models.profile import Profile
+
+
+def test_index():
+    """Get the index and test some things."""
+    client = Client()
+    response = client.get(reverse('index'))
+
+    if response.status_code != 200:
+        raise AssertionError()
